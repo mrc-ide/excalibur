97fd4e5c61c5a9562b1439760709c50fd42e77db
Greg Barnsley <gregbarnsley@hotmail.co.uk>
2021-06-08 16:52:07 +0100

    Add simulate method for epiModel objects

---
+++ tests/testthat/test-simulate_epiModel_method.R
@@ -0,0 +1,19 @@
+test_that("Check error is functioning", {
+  model <- setSIR(N= 10, Beta=1, Gamma=1/5, ProbOfDeath = 0, I0 = 1,
+                  changeTimes = NULL)
+  expect_error(simulate(model, t=1))
+})
+
+test_that("Compare simulate method to $run", {
+
+  model <- setSIR(N= 10, Beta=1, Gamma=1/5, ProbOfDeath = 0, I0 = 1,
+                  changeTimes = NULL)
+  expect_equal(simulate(model, t = c(1,2)), as.data.frame(model@odinModel$run(c(1,2))))
+  expect_equal(simulate(model, t = c(1,2,100)), as.data.frame(model@odinModel$run(c(1,2,100))))
+
+
+  model <- setSIR(N= 10, Beta=c(1,5), Gamma=1/5, ProbOfDeath = 0.5, I0 = 1,
+                  changeTimes = 12)
+  expect_equal(simulate(model, t = c(1,2)), as.data.frame(model@odinModel$run(c(1,2))))
+  expect_equal(simulate(model, t = c(1,2,100)), as.data.frame(model@odinModel$run(c(1,2,100))))
+})
