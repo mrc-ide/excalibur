9349e1891517cda1520423760ed8e0a1950bc5fb
Greg Barnsley <gregbarnsley@hotmail.co.uk>
2021-06-08 09:31:46 +0100

    Setup package rOxygen Documentation

---
+++ R/excalibur.R
@@ -0,0 +1,10 @@
+#' excalibur: A package for looking at the calibrations in ODE epidemic models
+#'
+#' The excalibur package is work in progress
+#'
+#' @section excalibur functions:
+#' setSIR: A function that produces a model of the SIR class in odin
+#'
+#' @docType package
+#' @name excalibur
+NULL
